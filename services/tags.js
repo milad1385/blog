98cfb091f3fd49d9a8f42a56077740bd7d20b1cf
Milad salamian <miladsalami1385@gmail.com>
2024-12-01 03:28:07 -0800

    add delete , findById , findAll service for tags

--- services/tags.js
+++ services/tags.js
@@ -16,4 +16,38 @@ exports.create = async (title) => {
   }
 };
 
+exports.findById = async (id) => {
+  try {
+    const query = "SELECT * FROM tags WHERE id = ?";
 
+    const [tag] = await db.execute(query, [id]);
+
+    return tag[0];
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.findAll = async () => {
+  try {
+    const query = "SELECT * FROM tags";
+
+    const [tags] = await db.execute(query);
+
+    return tags;
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.delete = async (id) => {
+  try {
+    const query = "DELETE FROM tags WHERE id = ?";
+
+    await db.execute(query, [id]);
+
+    return true;
+  } catch (error) {
+    next(error);
+  }
+};
