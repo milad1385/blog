5b04a4f0645b5e867f4340d22fbfee169c797d38
Milad salamian <miladsalami1385@gmail.com>
2024-12-03 09:50:48 -0800

    update create new article migration

--- migrations/articles-ddl.sql
+++ migrations/articles-ddl.sql
@@ -2,6 +2,7 @@ CREATE TABLE articles (
     id int(10) unsigned NOT NULL AUTO_INCREMENT,
     title varchar(255) NOT NULL,
     content mediumtext  NOT NULL,
+    cover varchar(255),
     slug varchar(255) NOT NULL,
     author_id int(10) unsigned NOT NULL,
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
--- routes/article.routes.js
+++ routes/article.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const controller = require("../controllers/article.controller");
+const { multerStorage } = require("../middlewares/multer");
 const authGuard = require("../middlewares/authGuard");
 const router = express.Router();
 
-router.route("/").post(authGuard, controller.create);
+const multer = multerStorage("public/images/article");
+
+router.route("/").post(authGuard, multer.single("cover"), controller.create);
 
 router.route("/remove/:id").post(authGuard, controller.delete);
-
-
 
 module.exports = router;
